60825809962aceeffaa1470012166a49590b4688
winnie-webb <71949190+winnie-webb@users.noreply.github.com>
2024-10-14 13:31:40 -0500

    Added basic lazy loading

--- app/components/MovieCard.jsx
+++ app/components/MovieCard.jsx
@@ -1,17 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
-// components/MovieCard.jsx
 import Link from "next/link";
 import React from "react";
 
 const MovieCard = ({ title, imageUrl, link }) => {
-  console.log(link.split("/"));
   return (
     <Link href={`/${link.split("/")[3]}`}>
       <div className="">
         <img
           className="w-full h-60 object-cover rounded-xl"
-          src={imageUrl}
+          data-src={imageUrl}
+          src="/loading.jpg"
           alt={title}
+          loading="lazy"
+          onLoad={(e) => {
+            console.log(e.target);
+            e.target.src = e.target.getAttribute("data-src");
+          }}
         />
       </div>
     </Link>
