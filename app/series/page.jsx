7e07c73a0e2066b6a2e2c5c325877e2769109cbe
winnie-webb <71949190+winnie-webb@users.noreply.github.com>
2024-10-15 11:35:05 -0500

    Added Series component and route

---
+++ app/series/page.jsx
@@ -0,0 +1,27 @@
+"use client";
+import MovieList from "../components/MovieList";
+import SkeletonLoader from "../components/SkeletonLoader";
+import useFetchGenres from "../custom-hooks/useFetchGenres";
+export default function Home() {
+  const { movies, loading, error, genres } = useFetchGenres("series");
+  if (error) {
+    return <div>Error: {error}</div>; // Error handling
+  }
+
+  return (
+    <div>
+      <div className="bg-cover bg-[url('/hero-1.png')] bg-center bg-no-repeat w-full h-[30vh] md:h-[40vh] xl:h-[75vh] "></div>
+      <div className="p-4 md:p-10">
+        {genres.map((genre) => (
+          <div key={genre} className="mb-8">
+            {loading ? (
+              <SkeletonLoader title={genre} />
+            ) : (
+              <MovieList title={genre} movies={movies[genre] || []} />
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
