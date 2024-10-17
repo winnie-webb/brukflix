fab2da73e26a4a1e61b1d908c63d93cfd6225ec7
winnie-webb <71949190+winnie-webb@users.noreply.github.com>
2024-10-17 16:37:44 -0500

    Fixed issues for local development regrading npm run dev

--- app/api/utils/getBrowser.js
+++ app/api/utils/getBrowser.js
@@ -1,12 +1,20 @@
 import puppeteer from "puppeteer";
+import fs from "fs";
+
+// Check if running in Docker by checking for the existence of a file or env variable
+const isDocker = fs.existsSync("/.dockerenv") || process.env.IS_DOCKER;
 
 async function getBrowser() {
   const browser = await puppeteer.launch({
-    executablePath:
-      process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/chromium",
-    args: ["--no-sandbox", "--disable-setuid-sandbox"], // Disable sandbox
+    executablePath: isDocker
+      ? process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/chromium"
+      : undefined, // Use default Chromium in dev
+    args: isDocker
+      ? ["--no-sandbox", "--disable-setuid-sandbox"] // Docker-specific args
+      : [], // Dev-specific args
   });
 
   return browser;
 }
+
 export default getBrowser;
